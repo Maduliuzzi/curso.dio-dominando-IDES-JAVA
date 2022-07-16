0f983c17f5c55280a78bcadf39d9bff5105da6a6
madu <liuzzieduarda@gmail.com>
2022-07-15 23:00:20 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		/*System.out.println("Hello world");*/
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+	}
+
+}
+class Livros {
+	
+	private String nome;
+	private String npag;
+	
+}
